8e9218a723783f6d69e90d36fd92aec9e3790cff
Mimoh123 <mppalikhe@gmail.com>
2024-11-24 13:03:03 +0545

    fix:fixed the bug in resetOtp page

--- src/pages/auth/OtpPageReset.jsx
+++ src/pages/auth/OtpPageReset.jsx
@@ -182,6 +182,7 @@ const OtpPageReset = () => {
             <div className='flex'>
               <h1>{`Didn't receive a code?`}</h1>
               <button
+                type='button'
                 className='ml-2 text-blue-700 font-semibold'
                 onClick={handleResend}
               >
